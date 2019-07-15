d184fe4c0f148e536b26e16d0725175920363112
felix <2319979647@qq.com>
2019-07-15 12:40:25 +0800

    fix: bottom & title

---
+++ CleanMac/Pods/Target Support Files/Pods-CleanMac/Pods-CleanMac-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CleanMac : NSObject
+@end
+@implementation PodsDummy_Pods_CleanMac
+@end
---
+++ CleanMac/Pods/Target Support Files/Pods-CleanMacUITests/Pods-CleanMacUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CleanMacUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CleanMacUITests
+@end
